e4f8274322b9ec3be5879b0e390ef84b58a9ca86
Louis Bouré <louisboure@hotmail.fr>
2024-03-13 03:45:37 +0100

    [FIX] adapt Health to common

--- Component/Entity/Health.cs
+++ Component/Entity/Health.cs
@@ -1,21 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace com.IronicEntertainment.Scripts
 {
-    public class Health : MonoBehaviour
+    public class Health
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
     }
 }
